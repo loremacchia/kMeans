b16856221e452abc8ce5583b9c649d0d4965a136
loremacchia <loremacchia@gmail.com>
2021-02-05 16:58:26 +0100

    Changed 2d arrays in 1d arrays

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,31 +1,41 @@
-
 #include "rapidcsv.h"
 #include <math.h> 
 
-double** getDataset(int* lenght, int* dim);
-double** getFarCentroids(double **points, int pointsLength, int dimensions);
+double* getDataset(int* lenght, int* dim);
+double* getFarCentroids(double *points, int pointsLength, int dimensions);
 
 const int k = 3;
 int main(int argc, char const *argv[]) {
     int dataLength;
     int dimensions;
-    double **points = getDataset(&dataLength, &dimensions);
-    double **centroids = getFarCentroids(points, dataLength, dimensions);
+    double *points = getDataset(&dataLength, &dimensions);
+    double *centroids = getFarCentroids(points, dataLength, dimensions);
 
+    for (int i = 0; i < dataLength; i++) {
+        for (int j = 0; j < dimensions; j++) {
+            printf("%f ", points[i*dimensions + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < dimensions; j++) {
+            printf("%f ", centroids[i*dimensions + j]);
+        }
+        printf("\n");
+    }
+    
     double distanceFromOld = 0;
     do {
         int pointsInCluster[k]; 
-        double **newCentroids = new double*[k];
-        for(int j = 0; j < k; j++) {
-            newCentroids[j] = new double[dimensions];
-        }
+        double *newCentroids = new double[k*dimensions];
         for (int i = 0; i < dataLength; i++) {
             double dist = 100; // Updated distance from point to the nearest Cluster. Init with a big value. TODO check if it is enough
             int clustId = -1; // Id of the nearest Cluster
             for (int j = 0; j < k; j++) {
                 double newDist = 0; //Distance from each Cluster
                 for (int x = 0; x < dimensions; x++) {
-                    newDist += fabs(points[i][x] - centroids[j][x]);
+                    newDist += fabs(points[i*dimensions + x] - centroids[j*dimensions + x]);
                 }
                 if(newDist < dist) {
                     dist = newDist;
@@ -33,28 +43,28 @@ int main(int argc, char const *argv[]) {
                 }
             }
             for (int x = 0; x < dimensions; x++) {
-                newCentroids[clustId][x] += points[i][x];
+                newCentroids[clustId*dimensions + x] += points[i*dimensions + x];
                 pointsInCluster[clustId]++;
             }
         }
         distanceFromOld = 0;
         for (int j = 0; j < k; j++) {
             for (int x = 0; x < dimensions; x++) {
-                newCentroids[j][x] /= pointsInCluster[j];
+                newCentroids[j*dimensions + x] /= pointsInCluster[j];
             }
         }
         for (int j = 0; j < k; j++) {
             for (int x = 0; x < dimensions; x++) {
-                distanceFromOld += fabs(newCentroids[j][x] - centroids[j][x]);
+                distanceFromOld += fabs(newCentroids[j*dimensions + x] - centroids[j*dimensions + x]);
             }
         }
         for (int j = 0; j < k; j++) {
             for (int x = 0; x < dimensions; x++) {
-                centroids[j][x] = newCentroids[j][x];
+                centroids[j*dimensions + x] = newCentroids[j*dimensions + x];
             }
         }
         printf("%f\n",distanceFromOld);
-    } while (distanceFromOld > 0.000001);
+    } while (distanceFromOld > 0.0000001);
     
     return 0;
 }
@@ -62,16 +72,14 @@ int main(int argc, char const *argv[]) {
 
 
 
-double** getDataset(int* lenght, int* dim) {
+double* getDataset(int* lenght, int* dim) {
     rapidcsv::Document doc("./dataset.csv", rapidcsv::LabelParams(-1, -1));
     const int rows = int(doc.GetRowCount()) - k;
     const int dimensions = doc.GetColumnCount() - 1;
     *lenght = rows;
     *dim = dimensions;
     printf("%d\n",rows);
-    double **points = new double*[rows];
-    for(int i = 0; i < rows; i++)
-        points[i] = new double[dimensions];
+    double *points = new double[rows*dimensions];
     for(int i = 0; i < rows; i++) {
         std::vector<std::string> row = doc.GetRow<std::string>(i);  
         double *array = new double[dimensions];
@@ -79,7 +87,7 @@ double** getDataset(int* lenght, int* dim) {
         for(auto element : row) {
             if(index != dimensions) {
                 // std::cout << element << std::endl;
-                points[i][index] = std::atof(element.c_str());
+                points[i*dimensions + index] = std::atof(element.c_str());
             }
             index++;
         }
@@ -87,19 +95,26 @@ double** getDataset(int* lenght, int* dim) {
     return points;
 }
 
-double** getFarCentroids(double **points, int pointsLength, int dimensions) {
+double* getFarCentroids(double *points, int pointsLength, int dimensions) {
     // Init set of clusters picking a point from the set and the k - 1 points further wrt the chosen point.
     // Those will be the firts clustroids
-    double *reference = points[0]; // The first available point is chosen as reference
+    double reference[dimensions]; // The first available point is chosen as reference
+    for (int tmpIdx = 0; tmpIdx < dimensions; tmpIdx++) {
+        reference[tmpIdx] = points[tmpIdx];
+    }
+    
     double *distances = new double[k-1]; 
-    double **realPoints = new double*[k]; // Array containing the further points wrt reference
-    realPoints[k - 1] = reference;
+    double *realPoints = new double[k*dimensions]; // Array containing the further points wrt reference
+    for (int tmpIdx = 0; tmpIdx < dimensions; tmpIdx++) {
+        realPoints[(k-1)*dimensions + tmpIdx] = reference[tmpIdx];
+    }
+    
     int maxSize = k - 1; // Maximum size of the array and relative indexes
     // Get the k - 1 points
     for(int i = 0; i < pointsLength; i++){
         double dist = 0;
         for (int x = 0; x < dimensions; x++) {
-            dist += fabs(points[i][x] - reference[x]);
+            dist += fabs(points[i*dimensions + x] - reference[x]);
         }
         if(dist > distances[maxSize - 1]) { // If the distance is higher than the last element of the array
             int index = 0;
@@ -108,10 +123,14 @@ double** getFarCentroids(double **points, int pointsLength, int dimensions) {
             }
             for (int j = maxSize - 1; j > index; j--) {
                 distances[j] = distances[j - 1];
-                realPoints[j] = realPoints[j - 1];
+                for (int tmpIdx = 0; tmpIdx < dimensions; tmpIdx++) {
+                    realPoints[j*dimensions + tmpIdx] = realPoints[(j - 1)*dimensions + tmpIdx];
+                }
             }
             distances[index] = dist;
-            realPoints[index] = points[i];
+            for (int tmpIdx = 0; tmpIdx < dimensions; tmpIdx++) {
+                realPoints[index*dimensions + tmpIdx] = points[i*dimensions + tmpIdx];
+            }
         }
     }
     return realPoints;
