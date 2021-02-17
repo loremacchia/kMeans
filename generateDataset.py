7aad0c6d9ed04db8b9cb2535d2dca873b1dae453
AndreaLeonardo <48476092+AndreaLeonardo@users.noreply.github.com>
2021-02-18 00:18:26 +0100

    Update omp branch

--- generateDataset.py
+++ generateDataset.py
@@ -1,15 +1,19 @@
 import csv
 from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
 
-sampleNumber = 100
+
+sampleNumber = 10000
 centerNumber = 3
-dimensions = 2
+dimensions = 3
 
 X, y, centers = make_blobs(n_samples=sampleNumber, centers=centerNumber, n_features=dimensions, random_state=0, return_centers=True)
 
 with open('dataset.csv', mode='w') as dataset:
     dataset_writer = csv.writer(dataset, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for i in range(len(X)):
-        dataset_writer.writerow([X[i][0],X[i][1], y[i]])
+        dataset_writer.writerow([X[i][j] for j in range(dimensions)]+[y[i]])
     for el in centers:
-        dataset_writer.writerow([el[0], el[1]])
+        dataset_writer.writerow([el[k] for k in range(dimensions)])
+
+
